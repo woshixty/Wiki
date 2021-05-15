d5d183f545b227f9522c6d7ffb4ccc3e9599c2bb
xty <qyyzxty@icloud.com>
2021-05-15 22:46:46 +0800

    axios

---
+++ src/main/java/com/jiawa/wiki/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.jiawa.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2021/5/15
+ **/
+// 请求跨域
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 所有的当前站点的请求地址，都支持跨域访问
+                .allowedOrigins("*") // 所有的外部域都可跨域访问，如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
+                .allowedMethods(CorsConfiguration.ALL) // 当前站点支持的跨域请求类型是什么
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowCredentials(true) // 是否支持跨域用户凭证
+                .maxAge(3600); // 超时时长设置为1小时，时间单位是秒
+    }
+
+}
