5c0d972775051488c56047584f08f4d30fa60a3d
xty <qyyzxty@icloud.com>
2021-05-16 21:25:27 +0800

    添加拦截器

---
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,23 @@
+package com.jiawa.wiki.config;
+
+import com.jiawa.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import javax.annotation.Resource;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2021/5/16
+ **/
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**");
+    }
+}
