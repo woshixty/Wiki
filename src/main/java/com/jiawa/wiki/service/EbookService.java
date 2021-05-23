262e75f27b9eb459b90741498efd331917ec036c
woshixty <qyyzxty@icloud.com>
2021-05-23 15:22:23 +0800

    集成pagehelper

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,8 +1,11 @@
 package com.jiawa.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,16 +16,26 @@ import java.util.List;
  * 2021/4/8
  **/
 @Service
+@Slf4j
 public class EbookService {
 
     @Resource
     private EbookMapper ebookMapper;
 
     public List<Ebook> list(String name) {
+        //对遇到的第一个SQL起作用
+        PageHelper.startPage(1, 3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + name + "%");
-        return ebookMapper.selectAll();
+        if (name != null) {
+            criteria.andNameLike("%" + name + "%");
+        }
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        log.info("{}", pageInfo.getTotal());
+        log.info("{}", pageInfo.getPages());
+        return ebookList;
     }
 
 }
