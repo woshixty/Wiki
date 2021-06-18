ed7016845e71955f29024dfbcfd6e1b06243fa6e
woshixty <qyyzxty@icloud.com>
2021-06-18 19:12:19 +0800

    9-3用户基本的增删改查

--- src/main/java/com/jiawa/wiki/mapper/UserMapper.java
+++ src/main/java/com/jiawa/wiki/mapper/UserMapper.java
@@ -2,35 +2,30 @@ package com.jiawa.wiki.mapper;
 
 import com.jiawa.wiki.domain.User;
 import com.jiawa.wiki.domain.UserExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface UserMapper {
     long countByExample(UserExample example);
 
     int deleteByExample(UserExample example);
 
-    int deleteByPrimaryKey(String user);
+    int deleteByPrimaryKey(Long id);
 
     int insert(User record);
 
     int insertSelective(User record);
 
-    List<User> selectByExampleWithBLOBs(UserExample example);
-
     List<User> selectByExample(UserExample example);
 
-    User selectByPrimaryKey(String user);
+    User selectByPrimaryKey(Long id);
 
     int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByExample(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKeyWithBLOBs(User record);
 
     int updateByPrimaryKey(User record);
 }
